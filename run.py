125a80551ed25399d342dc0a510ab59cdb442f02
Alan Daiki Suga <110938759+daikisuga@users.noreply.github.com>
2025-06-11 20:01:46 -0300

    commit definição do ip

--- run.py
+++ run.py
@@ -7,8 +7,4 @@ from app import create_app
 app = create_app()
 
 if __name__ =='__main__':
-<<<<<<< Updated upstream
-    app.run(host='192.168.00.00', port=8000) #ip Pedro
-=======
-    app.run(host='192.168.15.12', port=8000)
->>>>>>> Stashed changes
+    app.run(host='192.168.0.158', port=8000)
