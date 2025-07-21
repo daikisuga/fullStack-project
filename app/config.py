88bce2bb081ae2931d38f0bda5d0b8f0b6df624c
Alan Suga <alan.suga9@gmail.com>
2025-07-21 10:46:55 -0300

    mudançad de db: hub_datasets para dataset_tcc_db

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'uploads')
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/hud_datasets'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/dataset_tcc_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'sua-chave-secreta'
