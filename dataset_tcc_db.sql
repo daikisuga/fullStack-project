f0abd30430507afe0f50460064dda0089c728bb0
Gustavo Gomes Barbosa <100044322+Gustbarbr@users.noreply.github.com>
2025-07-22 12:49:17 -0300

    Add files via upload

---
+++ dataset_tcc_db.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21/07/2025 às 15:49
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dataset_tcc_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `datasets`
+--
+
+CREATE TABLE `datasets` (
+  `id` varchar(8) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `criador` varchar(100) NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `caminho_arquivos` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `datasets`
+--
+ALTER TABLE `datasets`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
